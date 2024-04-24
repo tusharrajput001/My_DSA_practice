d9fb7e9fdac75a83483f1ad39ccf6ed7a443b3dc
tusharrajput001 <devoptushar@gmail.com>
2024-04-25 00:29:53 +0530

    bubble

---
+++ Arrays/BubbelSort/Bubblesort.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int n, int arr[]){
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<= i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n = 5;
+    int arr[n] = {40,12,43,23,56,342};
+    cout<<"array before sort : ";
+    for(int i=0;i<=n;i++){
+        cout << arr[i]<<" ";
+    }
+    cout<<endl;
+    SelectionSort(n,arr);
+    cout<<"array after sort : ";
+    for(int i=0;i<=n;i++){
+        cout << arr[i]<<" ";
+    }
+    return 0;
+}
