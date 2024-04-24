de6cf7cf404e0718390eb1a2d299e52c495df03d
tusharrajput001 <devoptushar@gmail.com>
2024-04-24 23:55:37 +0530

    first commit

---
+++ Arrays/SelectionSort/selectionsort.cpp
@@ -0,0 +1,35 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int n, int arr[]){
+    for(int i=0;i<=n-2;i++){// check till n-2
+        int min = i;//let min index 1st index
+        for(int j=i; j<=n-1; j++){  // loop starts till end
+            if(arr[j] < arr[min]){
+                min=j;// search for min index
+            } 
+        }
+        //swap with min
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+int main() {
+    int n = 5;
+    int arr[n] = {40,12,43,23,56,342};
+    cout<<"array before sort : ";
+    for(int i=0;i<=n;i++){
+        cout << arr[i]<<" ";
+    }
+    cout<<endl;
+    SelectionSort(n,arr);
+    cout<<"array after sort : ";
+    for(int i=0;i<=n;i++){
+        cout << arr[i]<<" ";
+    }
+    return 0;
+}
