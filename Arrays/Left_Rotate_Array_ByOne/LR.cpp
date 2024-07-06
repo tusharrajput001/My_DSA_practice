ccca6126297a39c98c83d6a4cbfd65a2d4a82bd4
tusharrajput001 <devoptushar@gmail.com>
2024-07-06 23:02:51 +0530

    LR by one

--- Arrays/Left_Rotate_Array_ByOne/LR.cpp
+++ Arrays/Left_Rotate_Array_ByOne/LR.cpp
@@ -1,18 +1,17 @@
 // Brute Force Approach     (TC -> O(n) , SC -> O(n))
 
-// #include <iostream>
+// #include<bits/stdc++.h>
 // using namespace std;
 
-// int main()
-// {
-//     int n = 5;
-//     int arr[] = {1, 2, 3, 4, 5};
+// void solve(int arr[], int n){
+
 //     int temp[n];
 //     for (int i = 1; i < n; i++)
 //     {
 //         temp[i-1] = arr[i];
 //     }
-//     temp[n-1] = arr[0];
+//     temp[n-1]= arr[0];
+
 //     for (int i = 0; i < n; i++)
 //     {
 //         cout << temp[i] << " ";
@@ -21,28 +20,41 @@
 
 // }
 
+// int main() {
+//   int n=5;
+
+//   int arr[]= {1,2,3,4,5};
+//   solve(arr, n);
+// }
 
 // Optimal Approach     (TC -> O(n) , SC -> O(1))
 
-// #include <iostream>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main()
-// {
-//     int n = 5;
-//     int arr[] = {1, 2, 3, 4, 5};
+void solve(int arr[], int n)
+{
+    int temp = arr[0];
 
-//     int temp = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
 
-//     for (int i = 0; i < n - 1; i++)
-//     {
-//         arr[i] = arr[i + 1];
-//     }
+    arr[n - 1] = temp;
 
-//     arr[n - 1] = temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << arr[i] << " ";
-//     }
-// }
+int main()
+{
+    int n = 5;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    solve(arr, n);
+}
+
+
