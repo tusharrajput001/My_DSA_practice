a27a760f481aa3e206354a0cd49d076bf7ea48f4
tusharrajput001 <devoptushar@gmail.com>
2024-07-06 22:27:50 +0530

    checkarrsort

--- Arrays/Check_arrayisSorted/checkarraysorted.cpp
+++ Arrays/Check_arrayisSorted/checkarraysorted.cpp
@@ -1,29 +1,69 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n = 6;
+//     int arr[n]= {1,2,3,4,5,11};
+//     bool sorted = false;
+
+//     for(int i=1; i<n; i++){
+//         if(arr[i] >= arr[i-1]){
+//             sorted = true;
+//         }
+//         else{
+//             sorted = false;
+//         }
+//     }
+
+//     if(sorted){
+//         cout << "Sorted";
+//     }
+//     else{
+//         cout << "not sorted";
+//     }
+
+//     return 0;
+
+// }
+
+
+
+// OPTIMAL APPROACH
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
+void checkArrSorted(int arr[], int n)
+{
+    bool isSorted = false;
 
-int main(){
-    int n = 6;
-    int arr[n]= {1,2,3,4,5,11};
-    bool sorted = false; 
-
-    for(int i=1; i<n; i++){
-        if(arr[i] >= arr[i-1]){
-            sorted = true;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            isSorted = true;
         }
-        else{
-            sorted = false;
+        else
+        {
+            isSorted = false;
         }
     }
 
-    if(sorted){         
-        cout << "Sorted";   
+    if (isSorted)
+    {
+        cout << "Sorted";
     }
-    else{
-        cout << "not sorted";
+    else
+    {
+        cout << "Not Sorted";
     }
+}
 
-    return 0;
+int main()
+{
+    int n = 4;
+    int arr[n] = {12, 24, 33, 96, 13};
 
-
+    checkArrSorted(arr, n);
 }
