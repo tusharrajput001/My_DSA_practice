38e4b8648860952175746c2e5c75564c50c23591
tusharrajput001 <devoptushar@gmail.com>
2024-07-07 23:50:33 +0530

    move zzeroos to end

--- Arrays/Move_zeros_toend/moveZerosToEnd.cpp
+++ Arrays/Move_zeros_toend/moveZerosToEnd.cpp
@@ -1,89 +1,92 @@
-
-
-
-// BRUTE FORCE APPROACH
-// #include<iostream>
-// #include<vector>
+// BRUTE
+// #include <bits/stdc++.h>
 // using namespace std;
 
-// int main(){
-//     int n = 8;
-//     int arr[n] = {10,0,34,42,0,53,12,0};
-
-//     //step 1  (here temp storing all non-zero values)
+// vector<int> moveZeros(int n, vector<int> arr)
+// {
+//     //create temp vector
 //     vector<int> temp;
+
+//     // store nonzero in temp
 //     for (int i = 0; i < n; i++)
 //     {
-//         if(arr[i] != 0){
+//         if (arr[i] != 0)
+//         {
 //             temp.push_back(arr[i]);
 //         }
 //     }
 
-//     //step 2  (storing temp values to array from starting)
+//     // nz size
 //     int nz = temp.size();
-//     for(int i=0; i<nz; i++){
+
+//     // move all nz to front in arr
+//     for (int i = 0; i < nz; i++)
+//     {
 //         arr[i] = temp[i];
 //     }
 
-//     // step 3
+//     // rest of the arr is 0
 //     for (int i = nz; i < n; i++)
 //     {
 //         arr[i] = 0;
 //     }
 
-//     //step 4 print array
-//     for (int i = 0; i < n; i++)
-//     {
-//         cout << arr[i] << " ";
-//     }
-    
-    
-    
-
+//     return arr;
 // }
 
+// int main()
+// {
+//     vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+//     int n = 10;
+//     vector<int> ans = moveZeros(n, arr);
+//     for (auto &it : ans)
+//     {
+//         cout << it << " ";
+//     }
+//     cout << '\n';
+//     return 0;
+// }
 
+// OPTIMAL
 
-
-// OPTIMAL APPROACH
-
-
-// BRUTE FORCE APPROACH
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n = 8;
-    int arr[n] = {10,0,34,42,0,53,12,0};
-
-
-    //step 1 if array is zero then it is J;
+vector<int> moveZeros(int n, vector<int> a)
+{
     int j = -1;
-    for(int i=0; i<n; i++){
-        if(arr[i] == 0){
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 0)
+        {
             j = i;
             break;
         }
     }
 
-    // i = j+1 and if array is not zero than swap with zero
-    for (int i = j+1; i < n; i++)
-    {
-       if(arr[i] != 0){
-        swap(arr[i] , arr[j]);
-        j++;
-       }
-    }
-    
+    if (j == -1)
+        return a;
 
-    //step 4 print array
-    for (int i = 0; i < n; i++)
+    for (int i = j + 1; i < n; i++)
     {
-        cout << arr[i] << " ";
+        if (a[i] != 0)
+        {
+            swap(a[i], a[j]);
+            j++;
+        }
     }
-    
-    
-    
+    return a;
+}
 
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans)
+    {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
 }
