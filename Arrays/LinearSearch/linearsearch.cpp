1200f263060b14fcadcdf39c188e161ce3d04620
tusharrajput001 <devoptushar@gmail.com>
2024-07-08 22:35:37 +0530

    union

--- Arrays/LinearSearch/linearsearch.cpp
+++ Arrays/LinearSearch/linearsearch.cpp
@@ -1,24 +1,22 @@
-#include<iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
-    int inp;
-    int flag =0;
-    int n = 5;
-    int arr[n]  = {12,34,65,22,83};
-    cout<<"Enter number to search -> ";
-    cin >> inp;
-    cout << endl;
+// int search(int arr[], int n, int num)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == num)
+//         {
+//             return i;
+//         }
+//     }
+// }
 
-    for(int i=0; i<n; i++){
-        if(arr[i] == inp){
-            cout << inp << " found at place " << i << " index";
-            flag = 1;
-        }
-    }
-
-    if(flag == 0){
-        cout<<"Not found ";
-    }
-
-}
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int num = 4;
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     int val = search(arr, n, num);
+//     printf("%d", val);
+// }
