44d2a813620e97a7f7ececd17408e47fd2e44030
tusharrajput001 <devoptushar@gmail.com>
2024-07-10 23:59:57 +0530

    sort 0 1 2

--- Arrays/Sort an array of 0's 1's & 2's/sort.cpp
+++ Arrays/Sort an array of 0's 1's & 2's/sort.cpp
@@ -1,57 +1,144 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Brute
+
+// // Using any type of sorting
+
+// // Better Approach
+// void sortArray(vector<int>& arr, int n) {
+
+//     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] == 0) cnt0++;
+//         else if (arr[i] == 1) cnt1++;
+//         else cnt2++;
+//     }
+
+//     //Replace the places in the original array:
+//     for (int i = 0; i < cnt0; i++) arr[i] = 0; // replacing 0's
+
+//     for (int i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1; // replacing 1's
+
+//     for (int i = cnt0 + cnt1; i < n; i++) arr[i] = 2; // replacing 2's
+
+// }
+
+// // Optimal approach
+
+// void sortArray(vector<int>& arr, int n){
+
+//     int low = 0, mid=0, high=n-1;
+
+//     while(mid <= high){
+//         if(arr[mid] == 0){
+//             swap(arr[low], arr[mid]);
+//             low++;
+//             mid++;
+//         }
+//         else if(arr[mid] == 1){
+//             mid++;
+//         }
+//         else{
+//             swap(arr[mid], arr[high]);
+//             high--;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n = 6;
+//     vector<int> arr = {0, 2, 1, 2, 0, 1};
+//     sortArray(arr, n);
+//     cout << "After sorting:" << endl;
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// BETTER APPROACH
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void sortArray(vector<int>& arr, int n)
+// {
+//     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0)
+//             cnt0++;
+//         else if (arr[i] == 1)
+//             cnt1++;
+//         else
+//             cnt2++;
+//     }
+
+//     for (int i = 0; i < cnt0; i++)
+//     {
+//         arr[i] = 0;
+//     }
+//     for (int i = cnt0; i < cnt0 + cnt1; i++)
+//     {
+//         arr[i] = 1;
+//     }
+//     for (int i = cnt0 + cnt1; i < n; i++)
+//     {
+//         arr[i] = 2;
+//     }
+// }
+
+// int main()
+// {
+//     int n = 6;
+//     vector<int> arr = {0, 2, 1, 2, 0, 1};
+//     sortArray(arr, n);
+//     cout << "After sorting:" << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// OPTIMAL USING DUTCH NATIONAL FLAG
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Brute
+void sortArray(vector<int> &arr, int n)
+{
 
-// Using any type of sorting
+    int low = 0, mid = 0, high = n - 1;
 
+    while (mid <= high)
+    {
 
-// Better Approach
-void sortArray(vector<int>& arr, int n) {
-
-    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == 0) cnt0++;
-        else if (arr[i] == 1) cnt1++;
-        else cnt2++;
-    }
-
-    //Replace the places in the original array:
-    for (int i = 0; i < cnt0; i++) arr[i] = 0; // replacing 0's
-
-    for (int i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1; // replacing 1's
-
-    for (int i = cnt0 + cnt1; i < n; i++) arr[i] = 2; // replacing 2's
-
-}
-
-
-
-
-// Optimal approach
-
-
-void sortArray(vector<int>& arr, int n){
-
-    int low = 0, mid=0, high=n-1;
-
-    while(mid <= high){
-        if(arr[mid] == 0){
+        if (arr[mid] == 0)
+        {
             swap(arr[low], arr[mid]);
             low++;
-            mid++;  
-        }
-        else if(arr[mid] == 1){
             mid++;
         }
-        else{
+
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+
+        else
+        {
             swap(arr[mid], arr[high]);
             high--;
         }
     }
 }
- 
 
 int main()
 {
@@ -59,7 +146,8 @@ int main()
     vector<int> arr = {0, 2, 1, 2, 0, 1};
     sortArray(arr, n);
     cout << "After sorting:" << endl;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
