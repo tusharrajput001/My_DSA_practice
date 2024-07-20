a1338c529daa957e27d9b39d7e35fec5c646f7b9
tusharrajput001 <devoptushar@gmail.com>
2024-07-21 00:46:42 +0530

    setMatrix

--- Arrays/Set_Matrix_zero/setMatrix.cpp
+++ Arrays/Set_Matrix_zero/setMatrix.cpp
@@ -49,20 +49,146 @@ using namespace std;
 
 // BETTER APPROACH
 
+// vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
+// {
+//     int col[m] = {0};
+//     int row[n] = {0};
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (matrix[i][j] == 0)
+//             {
+//                 row[i] = 1;
+//                 col[j] = 1;
+//             }
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (row[i] || col[j])
+//             {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+//     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
+
+//     cout << "The Final matrix is: n";
+//     for (auto it : ans)
+//     {
+//         for (auto ele : it)
+//         {
+//             cout << ele << " ";
+//         }
+//         cout << "n";
+//     }
+//     return 0;
+// }
+
+// Bruteforce approach
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void markRow(vector<vector<int>> &matrix, int n, int m, int i)
+// {
+//     for (int j = 0; j < m; j++)
+//     {
+//         if (matrix[i][j] != 0)
+//         {
+//             matrix[i][j] = -1;
+//         }
+//     }
+// }
+
+// void markCol(vector<vector<int>> &matrix, int n, int m, int j)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (matrix[i][j] != 0)
+//         {
+//             matrix[i][j] = -1;
+//         }
+//     }
+// }
+
+// vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             markRow(matrix, n, m, i);
+//             markCol(matrix, n, m, j);
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (matrix[i][j] == -1)
+//             {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+//     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
+
+//     cout << "The Final matrix is : n";
+//     for (auto it : ans)
+//     {
+//         for (auto ele : it)
+//         {
+//             cout << ele << " ";
+//         }
+//         cout << "n";
+//     }
+
+//     return 0;
+// }
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
 vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
 {
-    int col[m] = {0};
     int row[n] = {0};
+    int col[m] = {0};
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (matrix[i][j] == 0)
+            if (matrix[i][j] = 0)
             {
                 row[i] = 1;
                 col[j] = 1;
-            }       
+            }
         }
     }
 
