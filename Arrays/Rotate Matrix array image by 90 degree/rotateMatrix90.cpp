35298de9039f7e1a6feebdfa066717d4110c9edc
tusharrajput001 <devoptushar@gmail.com>
2024-07-21 22:25:43 +0530

    rotate matrix

--- Arrays/Rotate Matrix array image by 90 degree/rotateMatrix90.cpp
+++ Arrays/Rotate Matrix array image by 90 degree/rotateMatrix90.cpp
@@ -2,38 +2,38 @@
 using namespace std;
 
 // Brute force
-// vector<vector<int>> rotate(vector<vector<int>> &matrix)
-// {
+vector<vector<int>> rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    vector<vector<int>> rotated(n, vector<int>(n, 0)); // 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            rotated[j][n - i - 1] = matrix[i][j];
+        }
+    }
+    return rotated;
+}
+
+// void rotate(vector < vector < int >> & matrix) {
 //     int n = matrix.size();
-//     vector<vector<int>> rotated(n, vector<int>(n, 0)); // 
 //     for (int i = 0; i < n; i++)
 //     {
-//         for (int j = 0; j < n; j++)
+//         for (int j = 0; j < i; j++)
 //         {
-//             rotated[j][n - i - 1] = matrix[i][j];
+//             swap(matrix[i][j], matrix[j][i]);
 //         }
-//     }
-//     return rotated;
-// }
-
-void rotate(vector < vector < int >> & matrix) {
-    int n = matrix.size();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            swap(matrix[i][j], matrix[j][i]);
-        }
         
-    }
+//     }
 
-    for (int i = 0; i < n; i++)
-    {
-        reverse(matrix[i].begin(), matrix[i].end());
-    }
+//     for (int i = 0; i < n; i++)
+//     {
+//         reverse(matrix[i].begin(), matrix[i].end());
+//     }
     
 
-}
+// }
 int main() {
     vector < vector < int >> arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     rotate(arr);
