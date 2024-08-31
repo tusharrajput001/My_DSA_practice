04d95676ed6b24caf567711d837cdc906df75cd3
tusharrajput001 <devoptushar@gmail.com>
2024-08-31 23:20:58 +0530

    insertion

---
+++ Sorting/Insertion_sort/insertion.cpp
@@ -0,0 +1,38 @@
+// TC -> O(N) , O(1) SC
+// In insertion sort we take 1st element as j and check its all previous elements if it is > j then swap check till end. 
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertion_sort(arr, n);
+    cout << "After Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
