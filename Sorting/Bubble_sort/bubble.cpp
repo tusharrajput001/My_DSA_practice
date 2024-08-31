220bf51d36fa362151107be983fb6e892eb1dcf4
tusharrajput001 <devoptushar@gmail.com>
2024-08-31 23:11:29 +0530

    bubble sort

---
+++ Sorting/Bubble_sort/bubble.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Bubble sort works like we will get our arr sorted from last to start
+// we run an outer loop from lastelement to first element
+// and inner loop will iterate over full arr and we take j and j+1 and check if j > j+1 then swap.
+
+// TC -> O(N2) , SC -> O(1)
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    cout << "After Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
