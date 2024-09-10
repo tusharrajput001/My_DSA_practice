b011438ece7932d0c9c2c6e338d20ebc7813e0e8
tusharrajput001 <devoptushar@gmail.com>
2024-09-10 15:35:32 +0530

    moores voting algo

--- Arrays/majority element n by 3 times/Majority.cpp
+++ Arrays/majority element n by 3 times/Majority.cpp
@@ -80,3 +80,58 @@ int main()
     cout << "\n";
     return 0;
 }
+
+
+
+// Using Moore's voting algo 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0 , cnt2 = 0;
+        int el1 = 0, el2 = 0;
+
+        // Extended Moore's voting algorithm
+        for(int i=0; i<n; i++){
+            if(cnt1 == 0 && nums[i]!= el2){
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            if(cnt2 == 0 && nums[i] != el1){
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1){
+             cnt1++;
+            }
+            else if(nums[i] == el2){
+             cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        //list to store ans
+        vector<int> ls;
+        int threshold = n / 3;
+
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == el1)cnt1++;
+            if(nums[i] == el2)cnt2++;
+        }
+
+
+        if(cnt1 > threshold){
+        ls.push_back(el1);
+        }
+
+        if(cnt2 > threshold){
+         ls.push_back(el2);
+        }
+
+        return ls;
+    }
+};
