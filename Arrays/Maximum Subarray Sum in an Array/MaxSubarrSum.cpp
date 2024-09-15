496924dd00607fe3193ddec29d3f168d9ea8c902
tusharrajput001 <devoptushar@gmail.com>
2024-09-15 23:33:28 +0530

    max subar sum

--- Arrays/Maximum Subarray Sum in an Array/MaxSubarrSum.cpp
+++ Arrays/Maximum Subarray Sum in an Array/MaxSubarrSum.cpp
@@ -66,34 +66,36 @@
 // OPTIMAL APPROACH USING KADANE's ALGO
 
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-long long maxSubarraySum(int arr[], int n) {
-    long long maxi = LONG_MIN;
-    long long sum =  0;
+// long long maxSubarraySum(int arr[], int n) {
+//     long long maxi = LONG_MIN;
+//     long long sum =  0;
 
-    for (int i = 0; i < n; i++)
-    {
-       sum += arr[i];
+//     for (int i = 0; i < n; i++)
+//     {
+//        sum += arr[i];
 
-       if(sum > maxi ){
-            maxi = sum;
-       }
+//        if(sum > maxi ){
+//             maxi = sum;
+//        }
 
-       if(sum < 0){
-            sum = 0;
-       }
-    }
-    return maxi;
-}
+//        if(sum < 0){
+//             sum = 0;
+//        }
+//     }
+//     return maxi;
+// }
 
-int main()
-{
-    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    long long maxSum = maxSubarraySum(arr, n);
-    cout << "The maximum subarray sum is: " << maxSum << endl;
-    return 0;
-}
+// int main()
+// {
+//     int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     long long maxSum = maxSubarraySum(arr, n);
+//     cout << "The maximum subarray sum is: " << maxSum << endl;
+//     return 0;
+// }
+
+
 
