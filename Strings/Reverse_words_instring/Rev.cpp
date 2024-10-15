e83d33023bbb5dc7e82e9a05e713a44ca6370134
Tushar rajput <devoptushar@gmail.com>
2024-10-15 23:30:29 +0530

    reverse words in string

--- Strings/Reverse_words_instring/Rev.cpp
+++ Strings/Reverse_words_instring/Rev.cpp
@@ -1,33 +1,82 @@
-// Example 1:
-
-// Input: s = "Let's take LeetCode contest"
-// Output: "s'teL ekat edoCteeL tsetnoc"
-// Example 2:
-
-// Input: s = "Mr Ding"
-// Output: "rM gniD"
+#include <bits/stdc++.h>
+using namespace std;
 
 
-class Solution {
-public:
-    string reverseWords(string s) {
-        int n = s.size();
-        string ans = "";
-        int i = 0;
-        while(i<n){
-            string temp = "";
-            while(s[i] == ' ' && i<n){
-                i++;
-            }
-            while(s[i] !=  ' ' && i<n){
-                temp += s[i];
-                i++;
-            }
-            if(temp.size() > 0){
-                reverse(temp.begin(), temp.end());
-                ans += temp + ' ';
-            }}
-    ans.pop_back();
-    return ans;
+// remove multiple spaces also
+
+// string result(string s){
+//     int n = s.size();
+//         string ans = "";
+//         int i = 0;
+//         while(i<n){
+//             string temp = "";
+//             while(s[i] == ' ' && i<n){
+//                 i++;
+//             }
+//             while(s[i] != ' ' & i<n){
+//                 temp += s[i];
+//                 i++;
+//             }
+
+//             if(temp.size() > 0){
+//                 if(ans.size() == 0){
+//                     ans = temp;
+//                 }
+//                 else{
+//                     ans = temp + " " + ans;
+//                 }
+//             }
+//         }
+
+//     return ans;
+
+// }
+
+
+
+
+string result(string s)
+{
+    int left = 0, right = s.length() - 1;
+
+    string temp = "";
+    string ans = "";
+
+    while (left <= right)
+    {
+        char ch = s[left];
+        if (ch != ' ')
+        {
+            temp += s[left];
+        }
+        else if (ch == ' ')
+        {
+            if (ans != "")
+                ans = temp + " " + ans;
+            else
+                ans = temp;
+            temp = "";
+        }
+        left++;
     }
-};
+
+    if (temp != "")
+    {
+        if (ans != "")
+            ans = temp + " " + ans;
+        else
+            ans = temp;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string st = "TUF is great for interview preparation";
+    cout << "Before reversing words: " << endl;
+    cout << st << endl;
+    cout << "After reversing words: " << endl;
+    cout << result(st);
+    return 0;
+}
