fb25de3520c2954daad0a3905f69a53ae1862234
Tushar rajput <devoptushar@gmail.com>
2024-10-21 23:49:23 +0530

    beauty comments

--- Strings/Sum of beauty of all substrings/beauty.cpp
+++ Strings/Sum of beauty of all substrings/beauty.cpp
@@ -5,19 +5,23 @@ int beautySum(string s)
 {
     int ans = 0;
     int n = s.length();
+    // nested loops for substrings generation
     for (int i = 0; i < n; i++)
     {
-        map<char, int> mpp;
+        map<char, int> mpp;  // map to store an char and its freq of presence
         for (int j = i; j < n; j++)
         {
-            mpp[s[j]]++;
+            mpp[s[j]]++;  // store in map
+            // let least freq & most freq
             int least_freq = INT_MAX;
             int most_freq = INT_MIN;
+            // iterate map and get least freq and most freq from characters
             for (auto it : mpp)
             {
                 least_freq = min(least_freq, it.second);
                 most_freq = max(most_freq, it.second);
             }
+            // find the difference of Most freq and least freq 
             ans += most_freq - least_freq;
         }
     }
