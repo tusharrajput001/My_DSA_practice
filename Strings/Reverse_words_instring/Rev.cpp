aa70eab20d843eaf7d537a84c1f932defced15c3
Tushar rajput <devoptushar@gmail.com>
2024-10-22 00:12:48 +0530

    reverse string with spaces

--- Strings/Reverse_words_instring/Rev.cpp
+++ Strings/Reverse_words_instring/Rev.cpp
@@ -1,75 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // remove multiple spaces also
-
-// string result(string s){
-//     int n = s.size();
-//         string ans = "";
-//         int i = 0;
-//         while(i<n){
-//             string temp = "";
-//             while(s[i] == ' ' && i<n){
-//                 i++;
-//             }
-//             while(s[i] != ' ' & i<n){
-//                 temp += s[i];
-//                 i++;
-//             }
-
-//             if(temp.size() > 0){
-//                 if(ans.size() == 0){
-//                     ans = temp;
-//                 }
-//                 else{
-//                     ans = temp + " " + ans;
-//                 }
-//             }
-//         }
-
-//     return ans;
-
-// }
-
-
-
 
 string result(string s)
 {
-    int left = 0, right = s.length() - 1;
+    int n = s.size();
+    int left = 0, right = n - 1;
 
-    string temp = "";
     string ans = "";
 
     while (left <= right)
     {
+        string temp = "";
         char ch = s[left];
-        if (ch != ' ')
+        // deal with white spaces
+        while (s[left] == ' ' && left <= right)
+        {
+            left++;
+        }
+        //  if it contain words then add in temp
+        while (s[left] != ' ' && left <= right)
         {
             temp += s[left];
+            left++;
         }
-        else if (ch == ' ')
+
+        // then store in ans
+        if (temp.size() > 0)
         {
-            if (ans != "")
-                ans = temp + " " + ans;
-            else
+            if (ans.size() == 0)
+            {
                 ans = temp;
-            temp = "";
+            }
+            else
+            {
+                ans = temp + " " + ans;
+            }
         }
-        left++;
     }
-
-    if (temp != "")
-    {
-        if (ans != "")
-            ans = temp + " " + ans;
-        else
-            ans = temp;
-    }
-
     return ans;
 }
+
+// string result(string s)
+// {
+//     int left = 0, right = s.length() - 1;
+
+//     string temp = "";
+//     string ans = "";
+
+//     while (left <= right)
+//     {
+//         char ch = s[left];
+//         if (ch != ' ')
+//         {
+//             temp += s[left];
+//         }
+//         else if (ch == ' ')
+//         {
+//             if (ans != "")
+//                 ans = temp + " " + ans;
+//             else
+//                 ans = temp;
+//             temp = "";
+//         }
+//         left++;
+//     }
+
+//     if (temp != "")
+//     {
+//         if (ans != "")
+//             ans = temp + " " + ans;
+//         else
+//             ans = temp;
+//     }
+
+//     return ans;
+// }
 
 int main()
 {
