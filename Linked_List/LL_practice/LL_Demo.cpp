15a5e3abb8b3ff94150c90d38f838f13558f7a14
Tushar rajput <devoptushar@gmail.com>
2024-11-02 16:43:29 +0530

    LL Insertion

--- Linked_List/LL_practice/LL_Demo.cpp
+++ Linked_List/LL_practice/LL_Demo.cpp
@@ -34,18 +34,21 @@ using namespace std;
 
 class Node
 {
-public:
-    int data;   // data
-    Node *next; // Pointer
+    public:
+        int data;   // data
+        Node *next; // Pointer
 
     // constructor
-public:
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
+    public:
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
 };
+
+
+
 
 void InsertAtTail(Node *&tail, int d)
 {
@@ -54,6 +57,8 @@ void InsertAtTail(Node *&tail, int d)
     tail = tail->next;
 }
 
+
+
 void InsertAtHead(Node *&head, int d)
 {
     // new node create
@@ -61,6 +66,31 @@ void InsertAtHead(Node *&head, int d)
     temp->next = head;
     head = temp;
 }
+
+
+void InsertAtPosition(Node* &tail, Node *&head,int position, int d){
+    if(position == 1){
+        InsertAtHead(head, d);
+        return;
+    }
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt < position-1){
+        temp = temp -> next;
+        cnt++;
+    }
+    Node* nodeToInsert =  new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+    if(temp -> next == NULL){
+        InsertAtTail(tail,d);
+        return;
+    }
+
+}
+
+
 
 void print(Node *&head)
 {
@@ -73,24 +103,27 @@ void print(Node *&head)
     cout << endl;
 }
 
+
+
+
 int main()
 {
     // created a new node
     Node *node1 = new Node(10);
-    cout << node1->data << endl;
-    cout << node1->next << endl;
+    cout << node1->data << endl;    //  10
+    cout << node1->next << endl;    // 0x0 null 
 
     // head pointed to node1
-    Node *head = node1;
-    Node *tail = node1;
-    print(head);
-
+    Node *head = node1;  // head - 10 
     InsertAtHead(head, 12);
     InsertAtHead(head, 15);
     print(head);
 
+    Node *tail = node1;  // tail - 10
     InsertAtTail(tail, 20);
     InsertAtTail(tail, 25);
+    print(head);
 
+    InsertAtPosition(tail,head,5,30);  
     print(head);
 }
