3976b805ccf59bd79c82a4cbb928351cdaf58259
Tushar rajput <devoptushar@gmail.com>
2024-11-17 17:06:19 +0530

    Doubley Linked list practice

---
+++ Linked_List/2D_DoubleyLL/Array to DLL/ConverArrtoDLL.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+
+public:
+    Node(int data1, Node *next1, Node *prev1)
+    {
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+
+void print(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp-> data<< " ";
+        temp = temp ->next;
+    }
+    cout << endl;
+}
+
+
+Node* convertArrtoDLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+
+
+
+int main(){
+    vector<int> arr = {20,10,25,16,14};
+    Node* head = convertArrtoDLL(arr);
+    print(head);
+}
